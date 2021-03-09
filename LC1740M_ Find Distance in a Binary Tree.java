72d6670bb1617ad48bf61f0260081ee280a3a756
derrickweiruluo <49405889+derrickweiruluo@users.noreply.github.com>
2021-03-08 22:08:46 -0500

    Update LC1740M_ Find Distance in a Binary Tree.java
    cleaner coding style

--- LC1740M_ Find Distance in a Binary Tree.java
+++ LC1740M_ Find Distance in a Binary Tree.java
@@ -20,37 +20,25 @@ class Solution {
     }
     
     private int dist(TreeNode root, int target, int travel) {
-        if(root == null) {
-            return -1;
-        }
         
-        if(root.val == target) {
-            return travel;
-        }
+        if(root == null) return -1;
+        if(root.val == target) return travel;
         
         int travelLeft = dist(root.left, target, travel + 1);
-        if(travelLeft == -1) {
-            return dist(root.right, target, travel + 1);
-        }
+        if(travelLeft == -1)  return dist(root.right, target, travel + 1);
         
         return travelLeft;
     }
     
     private TreeNode LCA(TreeNode currNode, int p, int q) {
-        if(currNode == null) {
-            return null;
-        }
         
-        if(currNode.val == p || currNode.val == q ){
-            return currNode;
-        }
-        
+        if(currNode == null) return null;
+        if(currNode.val == p || currNode.val == q) return currNode;
+
         TreeNode left = LCA(currNode.left, p, q);
         TreeNode right = LCA(currNode.right, p, q);
         
-        if((left != null && right != null)) {
-            return currNode;
-        }
+        if((left != null && right != null)) return currNode; 
                 
         return left == null? right: left;
     }
